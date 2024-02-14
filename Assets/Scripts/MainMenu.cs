6fe80646a0ef5abe75c27f2df3dac27a0cf761f7
Christian-Ashley <Christianashley86@gmail.com>
2024-02-13 20:54:32 -0600

    dont know why its not working

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -21,4 +21,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("maze");
     }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
 }
